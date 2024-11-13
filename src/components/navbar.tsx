396302d605fcb6dcd1bfeeb32db2700ac2c00910
BrayanCrV <brayancvilla@gmail.com>
2024-11-13 00:18:48 -0600

    Version movil completa e inicio modificado

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,7 @@ import styles from "./navbar.module.css";
 const Navbar: React.FC = () => {
     return (
         <nav className={styles.navBar}>
-            <Link to="/Principal" className={styles.NavLink}>Inicio</Link>
+            <a href="/Principal" className={styles.NavLink}>Inicio</a>
             <Link to="/Chat" className={styles.NavLink}>Chat</Link>
             <Link to="/Guardados" className={styles.NavLink}>Guardados</Link>
             <Link to="/" className={styles.NavLink}>Cerrar sesion</Link>
